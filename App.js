00894d81c2d6d02f19d068095c178cb8d8ff12c6
Vlad <vladvv2m@gmail.com>
2022-08-05 17:52:25 +0300

    Added splash screen and icon.

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 
+import { useState, useEffect } from "react";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Ionicons } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
 
 import ConsumptionCalculator from "./screens/ConsumptionCalculator";
 import SplitBill from "./screens/SplitBill";
@@ -11,8 +14,34 @@ import SplitBill from "./screens/SplitBill";
 import { Colors } from "./constants/colors";
 
 const BottomTabs = createBottomTabNavigator();
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (!appIsReady) {
+    return null;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return (
     <>
       <StatusBar style="auto" />
